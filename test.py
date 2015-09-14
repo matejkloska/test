3abee8e387733c59e83950043473e2ccb49d27c0
Matej Kloska <matej.kloska@gmail.com>
2015-09-14 14:10:31 +0200

    Init version

---
+++ test.py
@@ -0,0 +1,10 @@
+inn = raw_input('Input string: ')
+
+palindrome = True
+
+for i in xrange(0, len(inn)/2):
+    if inn[i] != inn[len(inn)-i-1]:
+        palindrome = False
+        break
+
+print 'Palindrome?', palindrome
